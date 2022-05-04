380d674c7fadbc47d15c2bb51c5bc7b2c7f16cf5
shehu.bledar9@gmail.com <shehu.bledar9@gmail.com>
2022-05-04 11:33:22 +0200

    change init file

---
+++ src/navigator/PrivateRoute.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import axios from 'axios'
+
+import { getToken, isLoggedIn } from '../shared/utils'
+import { Navigate, useLocation, useNavigate } from 'react-router-dom'
+
+const PrivateRoute = ({children, pageName}) => {
+
+    let navigate = useNavigate()
+    let location = useLocation()
+
+    React.useEffect(() => {
+        const token = getToken()
+        if (token === null) {
+            axios.defaults.headers.common.Authorization = ''
+            localStorage.clear()
+            navigate('/login')
+        } else {
+            navigate(location.pathname)
+        }
+    
+
+    }, [location.pathname, navigate])
+
+    if (!isLoggedIn()) {
+        return <Navigate to={'/login'} state={{from: location}} />
+    }
+    
+    if (isLoggedIn()) {
+        return <Navigate to={'/not-found'} state={{from: location}} />
+    }
+    
+  return (
+    <>{children}</>
+  )
+}
+
+export default PrivateRoute
---
+++ src/navigator/routes.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Login from '../views/Login'
+
+export const paths =Object.freeze({
+    LOGIN: "/login"
+})
+
+const routeNames = Object.freeze({
+    LOGIN: 'Login'
+ })
+
+export const routes = [
+    {
+        name: routeNames.LOGIN,
+        path: paths.LOGIN,
+        protected: false,
+        element: <Login />
+    }
+];
