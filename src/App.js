02e0a148bc54d009039aa058ae9b2600515f2d0a
User <user@Users-MacBook-Pro-3.local>
2022-10-03 01:38:31 +0200

    router fix

--- src/App.js
+++ src/App.js
@@ -3,6 +3,19 @@ import { BrowserRouter } from 'react-router-dom'
 import Navigator from './navigator/navigator'
 import moment from 'moment'
 import 'moment/locale/it'
+import { createBrowserHistory } from 'history'
+
+const replaceHashPath = () => {
+  const history = createBrowserHistory()
+  const hash = history.location.hash
+  if (hash) {
+    const path = hash.replace(/^#/, '')
+    if (path) {
+      history.replace(path)
+    }
+  }
+}
+replaceHashPath()
 
 moment.locale('it')
 
